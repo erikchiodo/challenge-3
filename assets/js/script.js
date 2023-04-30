81e854f322ba600de015a2121a269d974dbaae36
erikchiodo <erikchiodo@gmail.com>
2023-04-30 19:22:17 -0400

    Updated if/else statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,35 +2,21 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var password_length = 0;
+  let password_length = 0;
 
   function passwordFunction() {
     password_length = Math.floor(
-      Number(
-        prompt("Please enter password length. Choose a number between 8 & 128.")
-      )
-    );
+      Number(prompt("Please enter password length. Choose a number between 8 & 128.")));
     console.log("password length", password_length);
   }
-
   passwordFunction();
 
   // Password Alert Messages
-  if ((password_length < 8 || password_length > 128) && password_length != 0) {
-    alert(
-      "***Incorrect Password Length*** Please refresh page and re-enter correct value. Thank you!"
-    );
-  }
-  if (password_length >= 8 && password_length <= 128) {
+    if (password_length < 8 || password_length > 128) {
+    alert("***Incorrect Password Length*** Please refresh page and re-enter correct value. Thank you!");
+    return;
+  } else {
     alert("Password length accepted. Please proceed");
-  }
-  //   If user inputs 0 or cancels, it will revert them to original prompt
-  if (password_length == 0) {
-    password_length = Math.floor(
-      Number(
-        prompt("Please enter password length. Choose a number between 8 & 128.")
-      )
-    );
   }
 
   // Password Object (for lowercase characters, upper characters, special characters, numbers)
@@ -43,39 +29,26 @@ function generatePassword() {
 
   var password_string = "";
   // Confirm statements (lower, upper, special, number)
-  if (
-    confirm(
-      "Do you want lowercase letters in your password? Press OK for yes and Cancel for no"
-    )
-  ) {
+  if (confirm("Do you want lowercase letters in your password? Press OK for yes and Cancel for no")) 
+    {
     password_string = password_string + password_details.lower;
+    console.log(password_string);
   }
-
-  if (
-    confirm(
-      "Do you want uppercase letters in your password? Press OK for yes and Cancel for no"
-    )
-  ) {
+  if (confirm("Do you want uppercase letters in your password? Press OK for yes and Cancel for no")) 
+  {
     password_string = password_string + password_details.upper;
+    console.log(password_string);
   }
-
-  if (
-    confirm(
-      "Do you want special characters in your password? Press OK for yes and Cancel for no"
-    )
-  ) {
+  if (confirm("Do you want special characters in your password? Press OK for yes and Cancel for no")) 
+  {
     password_string = password_string + password_details.special;
+    console.log(password_string);
   }
-
-  if (
-    confirm(
-      "Do you want numbers in your password? Press OK for yes and Cancel for no"
-    )
-  );
-  password_string = password_string + password_details.number;
-
-  //   For testing purposes
-  console.log(password_string);
+  if (confirm("Do you want numbers in your password? Press OK for yes and Cancel for no")) 
+  {
+    password_string = password_string + password_details.number;
+    console.log(password_string);
+  } else {alert("No characters selected!");}
 
   var password = "";
   // Loop to generate password
